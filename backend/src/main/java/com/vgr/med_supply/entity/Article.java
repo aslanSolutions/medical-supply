607997eb7c8c64d35682f7941a3b78b49ef06fdb
Aslan <zakaria@bovision.se>
2025-09-11 12:55:45 +0200

    Enable file-based H2 database for data persistence

---
+++ backend/src/main/java/com/vgr/med_supply/entity/Article.java
@@ -0,0 +1,20 @@
+package com.vgr.med_supply.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String unit;
+
+    private int count;
+}
