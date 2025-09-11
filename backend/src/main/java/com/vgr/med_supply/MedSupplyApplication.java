998c791d8ae6dc0fd1e9b24da3b0bc781e251e76
Aslan <zakaria@bovision.se>
2025-09-11 10:41:33 +0200

    Initialize spring-boot in the backend dir

---
+++ backend/src/main/java/com/vgr/med_supply/MedSupplyApplication.java
@@ -0,0 +1,14 @@
+package com.vgr.med_supply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSupplyApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedSupplyApplication.class, args);
+    }
+}
+
---
+++ backend/src/test/java/com/vgr/med_supply/MedSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgr.med_supply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
